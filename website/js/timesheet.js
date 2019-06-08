8c427cca031a9d6f3b791676f936c434cadc3bc3
Eli Schiff <elihschiff@gmail.com>
2019-06-08 16:29:00 +0000

    time totaling

--- website/js/timesheet.js
+++ website/js/timesheet.js
@@ -12,20 +12,23 @@ Vue.component('time-cell', {
       // times[time_slot] = d.getHours()+":"+d.getMinutes();
       times[time_slot].hour = d.getHours();
       times[time_slot].min = d.getMinutes();
+    },
+    removeTime: function(times, time_slot, day_of_week){
+      times[time_slot].hour = null;
+      times[time_slot].min = null;
     }
   },
   template:`
-    <td v-if="times[time_slot].hour">
+    <td v-if="times[time_slot].hour != null" style="width: 20%">
       <div class="input-group">
-        <select class="custom-select" v-model="times[time_slot].hour">
-          <option v-for="i in 24" :value="i-1">{{i-1}}</option>
-        </select>
-        <select class="custom-select" v-model="times[time_slot].min">
-          <option v-for="i in 60" :value="i-1">{{i-1}}</option>
-        </select>
+        <input class="form-control form-control-sm" type="text" v-model="times[time_slot].hour">
+        <input class="form-control form-control-sm" type="text" v-model="times[time_slot].min">
+        <div class="input-group-append">
+          <button class="btn btn-sm btn-outline-secondary" type="button" v-on:click="removeTime(times,time_slot,day_of_week)">X</button>
+        </div>
       </div>
     </td>
-    <td v-else :id="day_of_week + '-' + time_slot">
+    <td v-else :id="day_of_week + '-' + time_slot" style="width: 20%">
       <button type="button" class="btn btn-primary btn-sm" v-on:click="fillAsNow(times,time_slot,day_of_week)">Set Now</button>
     </td>
   `
@@ -34,49 +37,71 @@ Vue.component('time-cell', {
 var app = new Vue({
   el: '#app',
   data: {
-    daysOfTheWeek:["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
     workDayLength:8,
-
-    data:  {
+    times:  {
       "Monday":{
         "time_in": {hour:null,min:null},
         "lunch_start": {hour:null,min:null},
         "lunch_end": {hour:null,min:null},
-        "time_out": {hour:null,min:null}
+        "time_out": {hour:null,min:null},
+        "total":{hour:null,min:null}
       },
       "Tuesday":{
         "time_in": {hour:null,min:null},
         "lunch_start": {hour:null,min:null},
         "lunch_end": {hour:null,min:null},
-        "time_out": {hour:null,min:null}
+        "time_out": {hour:null,min:null},
+        "total":{hour:null,min:null}
       },
       "Wednesday":{
         "time_in": {hour:null,min:null},
         "lunch_start": {hour:null,min:null},
         "lunch_end": {hour:null,min:null},
-        "time_out": {hour:null,min:null}
+        "time_out": {hour:null,min:null},
+        "total":{hour:null,min:null}
       },
       "Thursday":{
         "time_in": {hour:null,min:null},
         "lunch_start": {hour:null,min:null},
         "lunch_end": {hour:null,min:null},
-        "time_out": {hour:null,min:null}
+        "time_out": {hour:null,min:null},
+        "total":{hour:null,min:null}
       },
       "Friday":{
         "time_in": {hour:null,min:null},
         "lunch_start": {hour:null,min:null},
         "lunch_end": {hour:null,min:null},
-        "time_out": {hour:null,min:null}
+        "time_out": {hour:null,min:null},
+        "total":{hour:null,min:null}
       },
     }
   },
   methods: {
     fillAsNow: function(day, timeSlot){
       var d = new Date();
-      app.data[day][timeSlot] = d.getHours()+":"+d.getMinutes();
+      app.times[day][timeSlot] = d.getHours()+":"+d.getMinutes();
     },
-    eightHourDay: function(day){
-
+    totalTime: function(times){
+      var totalh = 0;
+      var totalm = 0;
+      for(day in times){
+        console.log(day)
+        totalh += times[day].total.hour;
+        totalm += times[day].total.min;
+      }
+      return totalh+":"+totalm;
+    },
+    totalTimeDay: function(day){
+      if(day.time_in.hour == null || day.lunch_start.hour == null || day.lunch_end.hour == null || day.time_out.hour == null){
+        return null;
+      }
+      var hoursWorked = (day.lunch_start.hour - day.time_in.hour) + (day.time_out.hour - day.lunch_end.hour);
+      var minWorked = (day.lunch_start.min - day.time_in.min) + (day.time_out.min - day.lunch_end.min);
+      hoursWorked += Math.floor(minWorked/60)
+      minWorked %= 60;
+      day.total.hour = hoursWorked;
+      day.total.min = minWorked;
+      return hoursWorked+":"+minWorked;
     }
   }
 })
