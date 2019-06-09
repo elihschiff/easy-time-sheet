ec97b73db90e17595750b5ce62593a3aa2dfd2a6
Eli Schiff <elihschiff@gmail.com>
2019-06-09 15:06:42 -0400

    removed port var

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express')
 var bodyParser = require("body-parser");
 const app = express()
-const port = 3000
 
-
-app.listen(process.env.PORT || port)
+app.listen(process.env.PORT || 3000)
 
 app.use(express.static('website'))
 app.use(bodyParser.urlencoded({ extended: false }));
